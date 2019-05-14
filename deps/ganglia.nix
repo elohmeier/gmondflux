5833f27fe13ee6264ec228ec9972c554419c4c3a
Enno Lohmeier <enno@nerdworks.de>
2019-05-14 10:29:25 +0200

    use own mirror

--- deps/ganglia.nix
+++ deps/ganglia.nix
@@ -5,7 +5,8 @@ stdenv.mkDerivation {
   version = "3.7.2";
 
   src = fetchurl {
-    url = "mirror://sourceforge/ganglia/ganglia%20monitoring%20core/3.7.2/ganglia-3.7.2.tar.gz";
+#    url = "mirror://sourceforge/ganglia/ganglia%20monitoring%20core/3.7.2/ganglia-3.7.2.tar.gz";
+    url = "https://www.nerdworks.de/aix/ganglia-3.7.2.tar.gz";
     sha256 = "042dbcaf580a661b55ae4d9f9b3566230b2232169a0898e91a797a4c61888409";
   };
 
